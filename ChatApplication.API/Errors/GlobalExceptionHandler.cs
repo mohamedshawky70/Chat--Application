385d49e8e1de9ade91b2147bd324a195618052e7
m.shawqi <mohmedzeedan2222@gmail.com>
2025-11-26 15:18:27 +0200

    Add logging, log to file.

--- ChatApplication.API/Errors/GlobalExceptionHandler.cs
+++ ChatApplication.API/Errors/GlobalExceptionHandler.cs
@@ -3,23 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ChatApplication.API.Errors;
 
-public class GlobalExceptionHandler : IExceptionHandler
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
 {
-	private readonly ILogger<GlobalExceptionHandler> _logger;
-	public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
-	{
-		_logger = logger;
-	}
+	private readonly ILogger<GlobalExceptionHandler> _logger = logger;
 
 	public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
 	{
-		_logger.LogError(exception, "Something went wrong {Message}", exception.Message);
+		_logger.LogError(exception, "Something went wrong {Message}", exception.Message);// Log the exception in cmd or any logging provider not in response
 		var problemDetails = new ProblemDetails()
 		{
 			Status = StatusCodes.Status500InternalServerError,
 			Title = "Internal server error",
 			Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1" //Link to RFC for 500 error to explain the error
 		};
+		//var result=Result.Failure<bool>(UserError.Ex); //If you don't want problemDetails
 		httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError; //Set all responses to 500
 		await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken); //Write the problem details as JSON response
 		return true;
